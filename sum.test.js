2fd3ee7263bd885828ea560147b7f78b06d37594
Luis <luis@rocketlab.com.br>
2020-05-15 12:52:01 -0300

    First commit tests jest and enzyme

---
+++ sum.test.js
@@ -0,0 +1,24 @@
+const sum = require('./sum');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+
+test('two plus two is four', () => {
+    expect(2 + 2).toBe(4);
+  });
+
+  test('Object assignment', () => {
+    const data = [1];
+    data.push(2);
+    expect(data).toEqual([1,2])
+  })
+
+  test('adding positive numbers is not zero', () => {
+    for (let a = 1; a < 10; a++) {
+      for (let b = 1; b < 10; b++) {
+        expect(a + b).not.toBe(0);
+      }
+    }
+  });
