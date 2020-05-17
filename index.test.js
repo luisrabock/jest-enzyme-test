bf580f8d1ae216e6b6f8e63d4d10c171a2ea9c98
Luis <luis@rocketlab.com.br>
2020-05-17 12:09:52 -0300

    test change files jest

--- index.test.js
+++ index.test.js
@@ -380,10 +380,10 @@ test('Custom matchers mock functions', () => {
   const arg2 = 'arg2';
 
   mockFunc();
+  mockFunc(arg1, arg2);
   // The mock function was called at least once
   expect(mockFunc).toHaveBeenCalled();
 
-  mockFunc(arg1, arg2);
   // The mock function was called at least once with the specified args
   expect(mockFunc).toHaveBeenCalledWith(arg1, arg2);
 
@@ -392,4 +392,32 @@ test('Custom matchers mock functions', () => {
 
   // All calls and the name of the mock is written as a snapshot
   expect(mockFunc).toMatchSnapshot();
+})
+
+test('common matchers', () => {
+  const mockFunc = jest.fn().mockName('a mock name');
+  const arg1 = 42;
+  const arg2 = 'arg2';
+
+  mockFunc(arg1, arg2);
+  // The mock function was called at least once
+  expect(mockFunc.mock.calls.length).toBeGreaterThan(0);
+
+  // The mock function was called at least once with the specified args
+  expect(mockFunc.mock.calls).toContainEqual([arg1, arg2]);
+
+  // The last call to the mock function was called with the specified args
+  expect(mockFunc.mock.calls[mockFunc.mock.calls.length - 1]).toEqual([
+    arg1,
+    arg2,
+  ]);
+
+  // The first arg of the last call to the mock function was `42`
+  // (note that there is no sugar helper for this specific of an assertion)
+  expect(mockFunc.mock.calls[mockFunc.mock.calls.length - 1][0]).toBe(42);
+
+  // A snapshot will check that a mock was invoked the same number of times,
+  // in the same order, with the same arguments. It will also assert on the name.
+  expect(mockFunc.mock.calls).toEqual([[arg1, arg2]]);
+  expect(mockFunc.getMockName()).toBe('a mock name');
 })
